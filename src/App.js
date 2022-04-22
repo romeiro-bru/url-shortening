5bb99eaca607d0bbed71feb8ed246bb2a7395163
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-04-22 20:37:17 -0300

    Add input field

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Url Shortening</h1>
+      <main>
+        <div className="banner">
+          <h1>More than just shorter links</h1>
+          <p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+          <button>Get Started</button>
+        </div>
+
+        <section className="input-box">
+          <input type="text" placeholder="Shorten a link here..." />
+          <button>Shorten It!</button>
+        </section>
+      </main>
     </div>
   );
 }
