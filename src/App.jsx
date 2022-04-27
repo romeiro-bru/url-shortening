e8ea466ee2ed281aad1b85433cbe8e6d112f1a0a
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-04-27 14:44:25 -0300

    Add icons, Add new classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css';
 import { Footer } from './Components/Footer/Footer';
 import { Header } from './Components/Header/Header';
+import chart from './assets/images/chart.png';
+import sparkles from './assets/images/sparkles.png';
+import paper from './assets/images/paper.png';
+
 
 const Banner = () => {
   return (
@@ -27,29 +31,45 @@ function App() {
       <main>
         <Header />
         <Banner />
-        <Input />
-        <section className="background">
-          <h1>Advanced Statistics</h1>
-          <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
-          <section className="description-box">
-            <div>
-              <h2>Brand Recognition</h2>
-              <p>Boost your brand recognition with each link.
-              Generic links don't mean a thing. Branded links help instil
-              confidence in your content.
-            </p>
+
+
+        <section className="container-description position-relative ">
+          <Input />
+          <div className="description-title">
+            <h1>Advanced Statistics</h1>
+            <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+          </div>
+
+          <section className="description-section">
+            <div className="position-relative">
+              <img src={chart} alt="icon" />
+              <div className="description-box">
+                <h2>Brand Recognition</h2>
+                <p>Boost your brand recognition with each link.
+                Generic links don't mean a thing. Branded links help instil
+                confidence in your content.
+              </p>
+              </div>
             </div>
-            <div>
-              <h2>Detailed Records</h2>
-              <p>Gain insights into who is clicking your links.
-              Knowing when and where people engage with your content helps inform better decisions.
+
+            <div className="position-relative">
+              <img src={paper} alt="icon" />
+              <div className="description-box">
+                <h2>Detailed Records</h2>
+                <p>Gain insights into who is clicking your links.
+                Knowing when and where people engage with your content helps inform better decisions.
             </p>
+              </div>
             </div>
-            <div>
-              <h2>Fully Customizable</h2>
-              <p>Improve brand awareness and content discoverabilitu through customizable links,
-              supercharging audience engagement.
+
+            <div className="position-relative">
+              <img src={sparkles} alt="icon" />
+              <div className="description-box">
+                <h2>Fully Customizable</h2>
+                <p>Improve brand awareness and content discoverabilitu through customizable links,
+                supercharging audience engagement.
             </p>
+              </div>
             </div>
           </section>
         </section>
