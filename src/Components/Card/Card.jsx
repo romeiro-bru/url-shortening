35ef321cfd8b5542364e88451f0d8e692848e61c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2022-04-28 19:52:24 -0300

    Receive props

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,18 +1,12 @@
 import './style.css';
-import chart from '../../assets/images/chart.png';
-import sparkles from '../../assets/images/sparkles.png';
-import paper from '../../assets/images/paper.png';
 
-export function Card() {
+export function Card({ title, content, img }) {
   return (
     <div className="position-relative">
-      <img src={chart} alt="icon" />
+      <img src={img} alt="icon" />
       <div className="card">
-        <h2>Brand Recognition</h2>
-        <p>Boost your brand recognition with each link.
-        Generic links don't mean a thing. Branded links help instil
-        confidence in your content.
-      </p>
+        <h2>{title}</h2>
+        <p>{content}</p>
       </div>
     </div>
   )
